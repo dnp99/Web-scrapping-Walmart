c2deea7925f7b45a413aa5150706110080945be2
deep patel <pateldeep1001@gmail.com>
2018-11-21 17:17:41 -0500

    web api first try

--- scrape.py
+++ scrape.py
@@ -14,17 +14,27 @@ class WebScrapper:
 
       def getProductUPC(self):
             # In order to get the page loaded using JavaScript, we need a web driver
-            chrome_options = Options()
-            chrome_options.binary_location = GOOGLE_CHROME_BIN
-            chrome_options.add_argument('--disable-gpu')
-            chrome_options.add_argument('--no-sandbox')
-            browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
+#            GOOGLE_CHROME_BIN = /app/.apt/opt/google/chrome/chrome
+#            chrome_options = Options()
+#            chrome_options.binary_location = GOOGLE_CHROME_BIN
+#            chrome_options.add_argument('--disable-gpu')
+#            chrome_options.add_argument('--no-sandbox')
+#            browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=chrome_options)
             #chrome_exec_shim = os.environ.get("GOOGLE_CHROME_BIN", "chromedriver")
             #self.selenium = webdriver.Chrome(executable_path=chrome_exec_shim)
             #driver = webdriver.Chrome()
             #browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, 
             #chrome_options=chrome_options)
-            
+            CHROMEDRIVER_PATH = "/app/.chromedriver/bin/chromedriver"
+
+            chrome_bin = os.environ.get('GOOGLE_CHROME_BIN', "chromedriver")
+            options = webdriver.ChromeOptions()
+            options.binary_location = chrome_bin
+            options.add_argument("--disable-gpu")
+            options.add_argument("--no-sandbox")
+            options.add_argument('headless')
+            options.add_argument('window-size=1200x600')
+            browser = webdriver.Chrome(executable_path=CHROMEDRIVER_PATH, chrome_options=options)
             browser.get(self.productListUrl)
             page = browser.execute_script("return document.documentElement.outerHTML")
             browser.close()
