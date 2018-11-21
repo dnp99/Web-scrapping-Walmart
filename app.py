ceccb26d31d8d2fb035382dbc50aa1fdf6dde7cd
deep patel <pateldeep1001@gmail.com>
2018-11-21 10:59:56 -0500

    message changed

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
 from flask import Flask
+from selenium import webdriver
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-      return 'Hello, World!'
+      return 'Hello, Deep!'
+@app.route('/app1/')
+def hello_world():
+      return 'Hello, Deep!'
 
 if __name__ == "__main__":
     app.run()
