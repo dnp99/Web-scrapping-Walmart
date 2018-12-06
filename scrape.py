78a1472bb15d9e823af525f3d08d215457c3ec08
deep patel <pateldeep1001@gmail.com>
2018-12-06 10:44:36 -0500

    web api first try

--- scrape.py
+++ scrape.py
@@ -2,7 +2,7 @@ from flask import jsonify
 from selenium import webdriver
 #from bs4 import BeautifulSoup as soup
 from selenium.webdriver.chrome.options import Options
-import bs4,requests
+import bs4
 import os
 
 
@@ -13,13 +13,7 @@ class WebScrapper:
       def __init__(self, upc):
             self.productListUrl = self.productListUrl + upc
     
-      def getAmazonPrice(productUrl):
-            res=requests.get(productUrl)
-            res.raise_for_status()
-            soup=bs4.BeautifulSoup(res.text,'html.parser')
-            ele=soup.select('#product-purchase-cartridge > div.pricing-shipping > div.pricing.wgrid-3w6.wgrid-2w4.marg-l-0 > div.price-current > div:nth-child(1)')
-            return ele[0].text.strip()
-    
+      
 
 
       def getProductUPC(self):
