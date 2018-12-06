48e0f11308d915eb05fc153a120264a6d350db3c
deep patel <pateldeep1001@gmail.com>
2018-12-06 15:01:10 -0500

    web api first try

--- scrape.py
+++ scrape.py
@@ -38,7 +38,7 @@ class WebScrapper:
             chrome_bin = os.environ.get('GOOGLE_CHROME_SHIM', None)
             options = webdriver.ChromeOptions()
             options.binary_location = chrome_bin
-            options.add_argument("--no-sandbox")
+            options.add_argument('--no-sandbox')
             options.add_argument('headless')
             options.add_argument('start-maximized'); 
             options.add_argument('disable-infobars'); 
