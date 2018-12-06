44dad8c9a6e6bc732ec08743fb3ec3933caf80a5
deep patel <pateldeep1001@gmail.com>
2018-12-06 09:59:20 -0500

    web api first try

--- scrape.py
+++ scrape.py
@@ -50,7 +50,7 @@ class WebScrapper:
 
             # Validation not in place yet
             print(container)
-            return jsonify({ "ERROR" : str(page_soup) })
+            return jsonify({ "ERROR" : str(page) })
 #            if len(container) > 0:
 #
 #                  if len(container) > 1:
